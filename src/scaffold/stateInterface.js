ad887d0ea094f0bf0797b9124e9d6c5f5a016078
Ivo Evans <ievans.storrie@gmail.com>
2022-01-06 16:43:13 +0000

    Provide better starting values

--- src/scaffold/stateInterface.js
+++ src/scaffold/stateInterface.js
@@ -1,4 +1,13 @@
-module.exports = {
-  get: async () => ([]),
-  set: async () => {},
+/* A state item must conform to the StateItem interface - see docs */
+
+const get = async () => {
+  const stateItems = [] // perform your own logic to get state
+  return stateItems
 }
+
+const set = async (stateItem) => {
+  const existingStateItems = await get()
+  // perform your own logic to set state items, and then insert the entire State
+}
+
+module.exports = { get, set }
