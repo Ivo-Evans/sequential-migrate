de310ef6884cc9fbaf7cb404afa3ddfb04e84b22
Ivo Evans <ievans.storrie@gmail.com>
2022-01-07 18:20:38 +0000

    Write tests for the StateModifier functions

---
+++ src/util/rollbackMigration.test.ts
@@ -0,0 +1,84 @@
+import { StateInterface, StateItem, State } from "../types";
+import rollbackMigration from "./rollbackMigration";
+import dynamicRequire from "./dynamicRequire";
+jest.mock("./dynamicRequire");
+const mockedDynamicRequire = jest.mocked(dynamicRequire, true);
+
+describe("rollbackMigration", () => {
+  beforeEach(() => {
+    mockedDynamicRequire.mockClear();
+    mockedDynamicRequire.mockImplementation(() => ({
+      down: jest.fn(),
+    }));
+  });
+  it("requires the migration by its name", async () => {
+    const stateItem: StateItem = {
+      name: "a",
+      description: "",
+      runAt: 1,
+    };
+
+    const stateInterface: StateInterface = {
+      get: jest.fn(async () => []),
+      set: jest.fn(),
+    };
+
+    await rollbackMigration(stateItem, stateInterface);
+
+    expect(dynamicRequire).toHaveBeenCalledTimes(1);
+    expect(dynamicRequire).toHaveBeenCalledWith(stateItem.name);
+  });
+
+  it("Calls the migration's 'down' method", async () => {
+    const down = jest.fn();
+    mockedDynamicRequire.mockImplementation(() => ({
+      down,
+    }));
+
+    const stateItem: StateItem = {
+      name: "a",
+      description: "",
+      runAt: 1,
+    };
+
+    const stateInterface: StateInterface = {
+      get: jest.fn(async () => []),
+      set: jest.fn(),
+    };
+
+    await rollbackMigration(stateItem, stateInterface);
+
+    expect(down).toHaveBeenCalledTimes(1);
+  });
+
+  it("calls the state interface's 'set' method with the new state", async () => {
+    const oldState: State = [
+      {
+        name: "a",
+        description: "b",
+        runAt: 0,
+      },
+      {
+        name: "b",
+        description: "",
+        runAt: 1,
+      },
+      {
+        name: "c",
+        description: "",
+        runAt: 2,
+      },
+    ];
+
+    const set = jest.fn();
+    const stateInterface: StateInterface = {
+      get: jest.fn(async () => oldState),
+      set,
+    };
+
+    await rollbackMigration(oldState[0], stateInterface);
+
+    expect(set).toHaveBeenCalledTimes(1);
+    expect(set).toHaveBeenLastCalledWith(oldState.slice(1, 3));
+  });
+});
---
+++ src/util/runMigration.test.ts
@@ -0,0 +1,94 @@
+import { StateInterface, StateItem, State } from "../types";
+import runMigration from "./runMigration";
+import dynamicRequire from "./dynamicRequire";
+jest.mock("./dynamicRequire");
+const mockedDynamicRequire = jest.mocked(dynamicRequire, true);
+
+describe("Run migration", () => {
+  beforeEach(() => {
+    mockedDynamicRequire.mockClear();
+    mockedDynamicRequire.mockImplementation(() => ({
+      up: jest.fn(),
+    }));
+  });
+
+  it("requires the migration by its name", async () => {
+    const stateItem: StateItem = {
+      name: "a",
+      description: "",
+      runAt: 1,
+    };
+
+    const stateInterface: StateInterface = {
+      get: jest.fn(async () => []),
+
+      set: jest.fn(),
+    };
+
+    await runMigration(stateItem, stateInterface);
+
+    expect(dynamicRequire).toHaveBeenCalledTimes(1);
+    expect(dynamicRequire).toHaveBeenCalledWith(stateItem.name);
+  });
+
+  it("Calls the migration's 'up' method", async () => {
+    const up = jest.fn();
+    mockedDynamicRequire.mockImplementation(() => ({
+      up,
+    }));
+
+    const stateItem: StateItem = {
+      name: "a",
+      description: "",
+      runAt: 1,
+    };
+
+    const stateInterface: StateInterface = {
+      get: jest.fn(async () => []),
+      set: jest.fn(),
+    };
+
+    await runMigration(stateItem, stateInterface);
+
+    expect(up).toHaveBeenCalledTimes(1);
+  });
+
+  it("calls the state interface's 'set' method with the new state", async () => {
+    jest.spyOn(global.Date, "now").mockImplementationOnce(() => 10);
+
+    const oldState: State = [
+      {
+        name: "b",
+        description: "",
+        runAt: 0,
+      },
+    ];
+    const set = jest.fn();
+    const stateInterface: StateInterface = {
+      get: jest.fn(async () => oldState),
+      set,
+    };
+
+    const stateItem: StateItem = {
+      name: "a",
+      description: "",
+      runAt: 1,
+    };
+
+    await runMigration(stateItem, stateInterface);
+
+    expect(set).toHaveBeenCalledTimes(1);
+    expect(set).toHaveBeenLastCalledWith([
+      {
+        name: "b",
+        description: "",
+        runAt: 0,
+      },
+      {
+        name: "a",
+        description: "",
+        runAt: 10,
+      },
+    ]);
+  });
+});
