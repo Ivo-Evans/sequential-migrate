521fb4f3f9efb6300ea6ec1c155ed5ee53e8def9
Ivo Evans <ievans.storrie@gmail.com>
2022-01-07 17:03:12 +0000

    Call correct function

--- src/util/rollbackMigration.ts
+++ src/util/rollbackMigration.ts
@@ -3,7 +3,7 @@ import dynamicRequire from "./dynamicRequire";
 
 const rollbackMigration: StateModifier = async (stateItem, stateInterface) => {
   const migrationScript = dynamicRequire(stateItem.name);
-  await migrationScript.up();
+  await migrationScript.down();
   const state = await stateInterface.get();
   const newState = state.filter(({ name }) => name !== stateItem.name);
   await stateInterface.set(newState);
