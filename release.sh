3b538382bce015c742b7eb6ddc90bfe98638f1d1
Ivo Evans <ievans.storrie@gmail.com>
2022-01-20 10:43:39 +0000

    Write release script

--- release.sh
+++ release.sh
@@ -1,9 +1,21 @@
-#! usr/bin/bash
+#!/bin/bash
 
-# check if git repo is clean - abort if not
-# take version from argument - abort if not present
-# run build
-# run docs
-# commit -m "build before release"
-# npm version with version from argument
-# push?
+git diff --cached --exit-code
+if [[ $? -ne 0 ]] ;
+ then echo "unclean repository - abort" && exit 1; 
+fi;
+
+git branch | grep -q "* main"
+if [[ $? -ne 0 ]] ;
+ then echo "not on main branch - abort" && exit 1; 
+fi;
+
+if [[ $1 != patch && $1 != minor && $1 != major ]] ;
+then  echo "valid release type not provided" && exit 1;
+fi;
+
+npm run build;
+npm run docs;
+git add -A;
+git commit -m "pre-release build";
+npm version $1;
