b71fbcf1785ce2fee815253887e2290ad8898265
Ivo Evans <ievans.storrie@gmail.com>
2022-01-20 11:17:51 +0000

    pre-release build

--- build/types/exportedFunctions.types.d.ts
+++ build/types/exportedFunctions.types.d.ts
@@ -4,7 +4,7 @@ import { InferredState } from ".";
  *
  * `up` halts and exits if it encounters a migration with status {@link MIGRATION_STATUS.MISSING | missing} or {@link MIGRATION_STATUS.SKIPPED | skipped}, running all migrations it has already encountered.
  *
- * `up` runs {@link MigrationScript.up} before {@link StateScript.add}. This means that your first migration can be the one that creates the migration table or collection in your database. Just make sure that {@link StateScript.get} can handle a table that doesn't exist.
+ * `up` runs {@link MigrationScript.up} before {@link StateScript.add}. This means that your first migration can be the one that creates the migration table or collection in your database. Just make sure that your {@link StateScript} can handle a table that doesn't exist.
  *
  *
  * @param to - the name of the migration to migrate up to
@@ -12,7 +12,7 @@ import { InferredState } from ".";
  */
 export declare type up = (to?: string) => Promise<void>;
 /**
- * Rolls back {@link MIGRATION_STATUS.RUN | run} migrations down to (but not including) the named migration. If no migration is provided, and there are no {@link MIGRATION_STATUS.MISSING | mising} or {@link MIGRATION_STATUS.SKIPPED | skipped} migrations, rolls back all migrations.
+ * Rolls back {@link MIGRATION_STATUS.RUN | run} migrations down to (but not including) the named migration. If no migration is provided, rolls back all migrations.
  *
  * The `down` script halts and exits if it encounters a migration with status {@link MIGRATION_STATUS.MISSING | missing} or {@link MIGRATION_STATUS.SKIPPED | skipped}, running all migrations it has already encountered.
  *
@@ -22,7 +22,7 @@ export declare type up = (to?: string) => Promise<void>;
  */
 export declare type down = (to?: string) => Promise<void>;
 /**
- * Reads the `migrations` folder and returns the {@link InferredState}. For convenience, booleans indicating whether there are {@link MIGRATION_STATUS.MISSING | missing} and {@link MIGRATION_STATUS.SKIPPED | skipped} migrations are also returned.
+ * Compares the `migrations` folder with the value returned by {@link StateScript.get} to return the {@link InferredState}. For convenience, booleans indicating whether there are {@link MIGRATION_STATUS.MISSING | missing} and {@link MIGRATION_STATUS.SKIPPED | skipped} migrations are also returned.
  @category Library Exports
  */
 export declare type getStatus = () => Promise<{
